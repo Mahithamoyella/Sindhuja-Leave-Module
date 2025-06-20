7f7d4f48c9ddd02e8c1b8347e4f0c8ad87febfee
moyellamahitha <moyellamahitha123@gmail.com>
2025-06-20 17:46:21 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3003;
+const port = 3053;
 
 // PostgreSQL database configuration
 const pool = new Pool({
